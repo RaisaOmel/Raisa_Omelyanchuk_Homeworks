10eaaaeedefc669dee03a96ff6b0ffa170a5e16e
RaisaOmel <omelraisa@mail.ru>
2024-02-22 22:48:24 +0300

    самостоятельная. сделана 6 задача

---
+++ IndependentWork_20240221/task_6.py
@@ -0,0 +1,28 @@
+#) Вы идете в путешествие, надо подсчитать сколько у денег у каждого студента.
+# Класс студен описан следующим образом:
+class Grup():
+    def __init__(self):
+        self.spis=[]
+
+    def add_grup(self,student_):
+        self.spis.append(student_)
+
+    def sum_grup(self):
+         return sum([elem.money for elem in self.spis])
+class Student():
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+
+grup=Grup()
+print('Выход из ввода =')
+print('Введите Имя студента в группе и через пробел сумму деньг.')
+for elem in iter(input,'='):
+    try:
+        lst=elem.split(' ')
+        print(f'Принято {lst[0]} с суммой {int(lst[1])}')
+        grup.add_grup(Student(lst[0],int(lst[1])))
+    except:
+        print('Ошибка ввод.')
+
+print(f'Группа едет с суммой {grup.sum_grup()}')
