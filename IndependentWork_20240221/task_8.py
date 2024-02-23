7919f4a49de9f8ac81432bf4dc909d18451584e9
RaisaOmel <omelraisa@mail.ru>
2024-02-23 23:42:18 +0300

    еще 2 задачи

--- IndependentWork_20240221/task_8.py
+++ IndependentWork_20240221/task_8.py
@@ -7,3 +7,52 @@
 (может быть несколько и брони может быть несколько. Также может быть ничего)
 Выведите весь арсенал робота на экран
 """
+
+class Robot:
+    def __init__(self,name):
+        self.name=name
+        self.weapon={}
+        self.ammunition={}
+
+    def get_weapon(self,weapon_):
+        self.weapon[weapon_.name]={'uron':weapon_.uron, 'strength':weapon_.strength}
+
+    def get_ammunition(self,ammunition_):
+        self.ammunition[ammunition_.name]={'uron':ammunition_.uron, 'strength':ammunition_.strength}
+
+    def info(self):
+        print(f'робот {self.name} имеет:')
+        for key,val in self.ammunition.items():
+             print(f"{key}: урон={val['uron']}, прочность={val['strength']}")
+        for key,val in self.weapon.items():
+            print(f"{key}: урон={val['uron']}, прочность={val['strength']}")
+class Weapon:
+    def __init__(self,name,uron,strength):
+        self.name=name
+        self.uron=uron
+        self.strength=strength
+
+    def get_weapon(self,robot):
+        robot.weapon[self.name]={'uron':self.uron, 'strength':self.strength}
+class Ammunition:
+    def __init__(self,name,uron,strength):
+        self.name=name
+        self.uron=uron
+        self.strength=strength
+
+    def get_ammunition(self, robot):
+        robot.weapon[self.name] = {'uron': self.uron, 'strength': self.strength}
+
+
+ammunition=Ammunition('броня',0,50)
+ammunition2=Ammunition('шлем',0,40)
+
+weapon=Weapon('меч',10,30)
+weapon2=Weapon('автомат',100,90)
+
+robot=Robot('Друид')
+ammunition.get_ammunition(robot)
+weapon.get_weapon(robot)
+robot.get_ammunition(ammunition2)
+robot.get_weapon(weapon2)
+robot.info()
--- IndependentWork_20240221/task_9.py
+++ IndependentWork_20240221/task_9.py
@@ -12,3 +12,56 @@ apple = Apple("sort", [a,b,c], 120, "apple")
 apple.clear()
 >>"apple очищен"
 """
+class Fruits:
+    def __init__(self,sort,vitamins,price,name):
+        if vitamins is None:
+            vitamins=[]
+        self.name=name
+        self.sort=sort
+        self.vitamins=vitamins
+        self.price=price
+
+    def clear(self):
+        pass
+
+    def cut(self):
+        pass
+    def get_vitamin(self,vitamin):
+        if vitamin:
+            if self.vitamins.count(vitamin)==0:
+                self.vitamins.append(vitamin)
+
+    def del_vitamin(self,vitamin):
+        if vitamin:
+            self.vitamins.remove(vitamin)
+class Apple(Fruits):
+    def cut(self):
+        print(f'{self.name} порезан')
+
+class Banana(Fruits):
+    def __init__(self,sort,vitamins,price,name,kalium=''):
+        super().__init__(sort,vitamins,price,name)
+        self.kalium=kalium
+
+    def clear(self):
+        print(f'{self.name} очищен')
+class Orange(Fruits):
+    def clear(self):
+        print(f'{self.name} очищен')
+
+class Mandarin(Fruits):
+    def clear(self):
+        print(f'{self.name} очищен')
+
+apple=Apple('цитрон',['a','d','c'], 8,'apple')
+apple.cut()
+apple.clear()
+banana=Banana('желтый',['c','e'], 5,'banana',0)
+banana.cut()
+banana.clear()
+mandarin=Mandarin('красный',['a','c'], 6,'mandarin')
+mandarin.cut()
+mandarin.clear()
+orange=Orange('зеленый',['a','c','e'], 7,'orange')
+orange.cut()
+orange.clear()
